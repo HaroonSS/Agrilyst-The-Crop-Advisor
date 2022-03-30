f303464ee8b4e9a17e88f6fd9016839111a16bd6
22HS108 <98251284+22HS108@users.noreply.github.com>
2022-03-30 12:20:55 +0530

    Update fertilizer.py

--- utils/fertilizer.py
+++ utils/fertilizer.py
@@ -1,6 +1,7 @@
 fertilizer_dic = {
         'NHigh': """The N value of soil is high and might give rise to weeds.
         <br/> Please consider the following suggestions:
+     <br/><br/>
              <style>
     table,
     th,
@@ -23,18 +24,18 @@ fertilizer_dic = {
   </tr>
   <tr>
     <td>3</td>
-<td><i>Plant nitrogen fixing plants</i> – planting vegetables that are in Fabaceae family like peas, beans and soybeans have the ability to increase nitrogen in your soil
+<td><i>Plant nitrogen fixing plants</i> – planting vegetables that are in Fabaceae family like peas, beans and soybeans have the ability to increase nitrogen in your soil.
 </td>
   </tr>
   <tr>
   <td>4</td>
-    <td>Plant ‘green manure’ crops like cabbage, corn and brocolli
+    <td>Plant ‘green manure’ crops like cabbage, corn and brocolli.
 </td>
 
   </tr>
   <tr>
     <td>5</td>
-<td><i>Use mulch (wet grass) while growing crops</i> - Mulch can also include sawdust and scrap soft woods
+<td><i>Use mulch (wet grass) while growing crops</i> - Mulch can also include sawdust and scrap soft woods.
 </td>
   </tr>
   <tr>
@@ -50,93 +51,279 @@ fertilizer_dic = {
 
         'Nlow': """The N value of your soil is low.
         <br/> Please consider the following suggestions:
-        <br/><br/> 1. <i>Add sawdust or fine woodchips to your soil</i> – the carbon in the sawdust/woodchips love nitrogen and will help absorb and soak up and excess nitrogen.
+        <br/><br/>
+             <style>
+    table,
+    th,
+    td {
+        border: 2px solid black;
+        color: black;
+        font-size: 25px;
+        font-weight: bold;
+    }
+    </style>
+<table style="width:100%">
+  <tr>
+    <td>1</td>
+    <td><i>Add sawdust or fine woodchips to your soil</i> – the carbon in the sawdust/woodchips love nitrogen and will help absorb and soak up and excess nitrogen.</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td><i>Plant crops that transfer nitrogen from air to soil </i> – tomatoes, corn, broccoli, cabbage and spinach are examples of plants that thrive off nitrogen and will suck the nitrogen dry.
+</td>
+  </tr>
+  <tr>
+    <td>3</td>
+<td><i>Watering </i> – soaking your soil with water will help leach the nitrogen deeper into your soil, effectively leaving less for your plants to use.
 
-        <br/>2. <i>Plant crops that transfer nitrogen from air to soil </i> – tomatoes, corn, broccoli, cabbage and spinach are examples of plants that thrive off nitrogen and will suck the nitrogen dry.
+</td>
+  </tr>
+  <tr>
+  <td>4</td>
+    <td><i>Sugar </i> – In multiple studies, it was shown that adding sugar to your soilNo do can help potentially increase the amount of nitrogen is your soil. Sugar is partially composed of carbon, an element which attracts and soaks up the nitrogen in the soil. This is similar concept to adding sawdust/woodchips which are high in carbon content.
 
-        <br/>3. <i>Watering </i> – soaking your soil with water will help leach the nitrogen deeper into your soil, effectively leaving less for your plants to use.
+</td>
 
-        <br/>4. <i>Sugar </i> – In multiple studies, it was shown that adding sugar to your soilNo do can help potentially increase the amount of nitrogen is your soil. Sugar is partially composed of carbon, an element which attracts and soaks up the nitrogen in the soil. This is similar concept to adding sawdust/woodchips which are high in carbon content.
+  </tr>
+  <tr>
+    <td>5</td>
+<td>Add high nitrogen fertilizer like ammonia to the soil.
 
-        <br/>5. Add high nitrogen fertilizer like ammonia to the soil.
+</td>
+  </tr>
+  <tr>
+    <td>6</td>
+    <td>Plant Nitrogen fixing plants like peas or beans.
+</td>
+      </tr>
+<tr>
+<td>7</td>
+<td><i>Use NPK fertilizers with high N value.</td>
+</tr>
+<tr>
+<td>8</td>
+<td><i>Leave soil as it is </i> – It may seem counter-intuitive, but if you already have plants that are producing lots of foliage, it may be best to let them continue to absorb all the nitrogen to amend the soil for your next crops.</td>
+</tr>
+<tr>
+<td>9</td>
+<td><i> Use nitrogen suppling bacteria </i> - Bacteria such as Azotobacter, Bacillus, Clostridium, and Klebsiella transfer nitrogen from air to soil.</td>
+</tr>
 
-        <br/>6. Plant Nitrogen fixing plants like peas or beans.
-
-        <br/>7. <i>Use NPK fertilizers with high N value.
-
-        <br/>8. <i>Leave soil as it is </i> – It may seem counter-intuitive, but if you already have plants that are producing lots of foliage, it may be best to let them continue to absorb all the nitrogen to amend the soil for your next crops
-
-	<br/>9. <i> Use nitrogen suppling bacteria </i> - Bacteria such as Azotobacter, Bacillus, Clostridium, and Klebsiella transfer nitrogen from air to soil""",
+    </table>""",
 
         'PHigh': """The P value of your soil is high.
         <br/> Please consider the following suggestions:
 
-        <br/><br/>1. <i>Avoid adding more fertilizer</i> – To correct problems caused by excessive soil phosphorus, begin by avoiding future phosphorus applications. Limiting the addition of manure will help reduce phosphorus being added.
+        <br/><br/>
+             <style>
+    table,
+    th,
+    td {
+        border: 2px solid black;
+        color: black;
+        font-size: 25px;
+        font-weight: bold;
+    }
+    </style>
+<table style="width:100%">
+  <tr>
+    <td>1</td>
+    <td><i>Avoid adding more fertilizer</i> – To correct problems caused by excessive soil phosphorus, begin by avoiding future phosphorus applications. Limiting the addition of manure will help reduce phosphorus being added.</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td><i>Use only phosphorus-free fertilizer</i> – if you can limit the amount of phosphorous added to your soil, you can let the plants use the existing phosphorus while still providing other key nutrients such as Nitrogen and Potassium. Find a fertilizer with numbers such as 10-0-10, where the zero represents no phosphorous.
 
-        <br/>2. <i>Use only phosphorus-free fertilizer</i> – if you can limit the amount of phosphorous added to your soil, you can let the plants use the existing phosphorus while still providing other key nutrients such as Nitrogen and Potassium. Find a fertilizer with numbers such as 10-0-10, where the zero represents no phosphorous.
+</td>
+  </tr>
+  <tr>
+    <td>3</td>
+<td><i>Watering your soil</i> – soaking your soil liberally will aid in driving phosphorous out of the soil. This is recommended as a last ditch effort.
+</td>
+  </tr>
+  <tr>
+  <td>4</td>
+    <td>Plant nitrogen fixing vegetables to increase nitrogen without increasing phosphorous (like beans and peas).
+</td>
 
-        <br/>3. <i>Watering your soil</i> – soaking your soil liberally will aid in driving phosphorous out of the soil. This is recommended as a last ditch effort.
+  </tr>
+  <tr>
+    <td>5</td>
+<td>Use crop rotations to decrease high phosphorous levels.
+</td>
+  </tr>
+  <tr>
+    <td>6</td>
+    <td>Affected plants need additional iron and zinc. However, simply adding iron and zinc to the soil will not work. Research shows that soil-applied iron is rapidly tied up by alkaline soils and its availability to plants is limited.
+</td>
+      </tr>
+<tr>
+<td>7</td>
+<td>Organic nitrogen sources or mulches are needed, use very low phosphorus products such as blood meal (as a nitrogen source) or pine bark mulch.</td>
+</tr>
 
-        <br/>4. Plant nitrogen fixing vegetables to increase nitrogen without increasing phosphorous (like beans and peas).
+</table>
 
-        <br/>5. Use crop rotations to decrease high phosphorous levels
-	<br/>6. Affected plants need additional iron and zinc. However, simply adding iron and zinc to the soil will not work. Research shows that soil-applied iron is rapidly tied up by alkaline soils and its availability to plants is limited.
-	<br/>7. Organic nitrogen sources or mulches are needed, use very low phosphorus products such as blood meal (as a nitrogen source) or pine bark mulch""",
+""",
 
         'Plow': """The P value of your soil is low.
         <br/> Please consider the following suggestions:
 
-        <br/><br/>1. <i>Bone meal</i> – a fast acting source that is made from ground animal bones which is rich in phosphorous.
+        <br/><br/>
 
-        <br/>2. <i>Rock phosphate</i> – a slower acting source where the soil needs to convert the rock phosphate into phosphorous that the plants can use.
+             <style>
+    table,
+    th,
+    td {
+        border: 2px solid black;
+        color: black;
+        font-size: 25px;
+        font-weight: bold;
+    }
+    </style>
+<table style="width:100%">
+  <tr>
+    <td>1</td>
+    <td><i>Bone meal</i> – a fast acting source that is made from ground animal bones which is rich in phosphorous.</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td><i>Rock phosphate</i> – a slower acting source where the soil needs to convert the rock phosphate into phosphorous that the plants can use.
+</td>
+  </tr>
+  <tr>
+    <td>3</td>
+<td><i>Phosphorus Fertilizers</i> – applying a fertilizer with a high phosphorous content in the NPK ratio (example: 10-20-10, 20 being phosphorous percentage).
+</td>
+  </tr>
+  <tr>
+  <td>4</td>
+    <td><i>Organic compost</i> – adding quality organic compost to your soil will help increase phosphorous content.</td>
+  </tr>
+  <tr>
+    <td>5</td>
+<td><i>Manure</i> – as with compost, manure can be an excellent source of phosphorous for your plants.</td>
+  </tr>
+  <tr>
+    <td>6</td>
+    <td><i>Clay soil</i> – introducing clay particles into your soil can help retain & fix phosphorus deficiencies.</td>
+      </tr>
+<tr>
+<td>7</td>
+<td><i>Ensure proper soil pH</i> – having a pH in the 6.0 to 7.0 range has been scientifically proven to have the optimal phosphorus uptake in plants.
+</td>
+</tr>
+<tr>
+<td>8</td>
+<td>If soil pH is low, add lime or potassium carbonate to the soil as fertilizers. Pure calcium carbonate is very effective in increasing the pH value of the soil.</td>
+</tr>
+<tr>
+<td>9</td>
+<td>If pH is high, addition of appreciable amount of organic matter will help acidify the soil. Application of acidifying fertilizers, such as ammonium sulfate, can help lower soil pH.</td>
+</tr>
 
-        <br/>3. <i>Phosphorus Fertilizers</i> – applying a fertilizer with a high phosphorous content in the NPK ratio (example: 10-20-10, 20 being phosphorous percentage).
+</table>
 
-        <br/>4. <i>Organic compost</i> – adding quality organic compost to your soil will help increase phosphorous content.
-
-        <br/>5. <i>Manure</i> – as with compost, manure can be an excellent source of phosphorous for your plants.
-
-        <br/>6. <i>Clay soil</i> – introducing clay particles into your soil can help retain & fix phosphorus deficiencies.
-
-        <br/>7. <i>Ensure proper soil pH</i> – having a pH in the 6.0 to 7.0 range has been scientifically proven to have the optimal phosphorus uptake in plants.
-
-        <br/>8. If soil pH is low, add lime or potassium carbonate to the soil as fertilizers. Pure calcium carbonate is very effective in increasing the pH value of the soil.
-
-        <br/>9. If pH is high, addition of appreciable amount of organic matter will help acidify the soil. Application of acidifying fertilizers, such as ammonium sulfate, can help lower soil pH""",
+""",
 
         'KHigh': """The K value of your soil is high.
         <br/> Please consider the following suggestions:
 
-        <br/><br/>1. <i>Loosen the soil</i> deeply with a shovel, and water thoroughly to dissolve water-soluble potassium. Allow the soil to fully dry, and repeat digging and watering the soil two or three more times.
+        <br/><br/>
 
-        <br/>2. <i>Sift through the soil</i>, and remove as many rocks as possible, using a soil sifter. Minerals occurring in rocks such as mica and feldspar slowly release potassium into the soil slowly through weathering.
+             <style>
+    table,
+    th,
+    td {
+        border: 2px solid black;
+        color: black;
+        font-size: 25px;
+        font-weight: bold;
+    }
+    </style>
+<table style="width:100%">
+  <tr>
+    <td>1</td>
+    <td><i>Loosen the soil</i> deeply with a shovel, and water thoroughly to dissolve water-soluble potassium. Allow the soil to fully dry, and repeat digging and watering the soil two or three more times.</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td><i>Sift through the soil</i>, and remove as many rocks as possible, using a soil sifter. Minerals occurring in rocks such as mica and feldspar slowly release potassium into the soil slowly through weathering.</td>
+  </tr>
+  <tr>
+    <td>3</td>
+<td>Stop applying potassium-rich commercial fertilizer.</td>
+  </tr>
+  <tr>
+  <td>4</td>
+    <td>Mix crushed eggshells, crushed seashells, wood ash or soft rock phosphate to the soil to add calcium. Mix in up to 10 percent of organic compost to help amend and balance the soil.</td>
+  </tr>
+  <tr>
+    <td>5</td>
+<td>Use NPK fertilizers with low K levels and organic fertilizers since they have low NPK values.</td>
+  </tr>
+  <tr>
+    <td>6</td>
+    <td>Grow a cover crop of legumes that will fix nitrogen in the soil. This practice will meet the soil’s needs for nitrogen without increasing phosphorus or potassium.</td>
+      </tr>
 
-        <br/>3. Stop applying potassium-rich commercial fertilizer. Apply only commercial fertilizer that has a '0' in the final number field. Commercial fertilizers use a three number system for measuring levels of nitrogen, phosphorous and potassium. The last number stands for potassium. Another option is to stop using commercial fertilizers all together and to begin using only organic matter to enrich the soil.
 
-        <br/>4. Mix crushed eggshells, crushed seashells, wood ash or soft rock phosphate to the soil to add calcium. Mix in up to 10 percent of organic compost to help amend and balance the soil.
+</table>
 
-        <br/>5. Use NPK fertilizers with low K levels and organic fertilizers since they have low NPK values.
-
-        <br/>6. Grow a cover crop of legumes that will fix nitrogen in the soil. This practice will meet the soil’s needs for nitrogen without increasing phosphorus or potassium.
 	""",
 
         'Klow': """The K value of your soil is low.
         <br/>Please consider the following suggestions:
 
-        <br/><br/>1. Mix in muricate of potash or sulphate of potash
+        <br/><br/>
 
-        <br/>2.<i> Wood ash </i> -can also be used, but make sure that you apply wood ash only lightly, as too much can burn your plants.
+             <style>
+    table,
+    th,
+    td {
+        border: 2px solid black;
+        color: black;
+        font-size: 25px;
+        font-weight: bold;
+    }
+    </style>
+<table style="width:100%">
+  <tr>
+    <td>1</td>
+    <td>Mix in muricate of potash or sulphate of potash.</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td><i> Wood ash </i> -can also be used, but make sure that you apply wood ash only lightly, as too much can burn your plants.</td>
+  </tr>
+  <tr>
+    <td>3</td>
+<td><i> Greensand </i> -which is available from most nurseries, will also add potassium to you garden.
+</td>
+  </tr>
+  <tr>
+  <td>4</td>
+    <td>Bury banana peels an inch below the soils surface.</td>
+  </tr>
+  <tr>
+    <td>5</td>
+<td>Use Potash fertilizers since they contain high values potassium.
+</td>
+  </tr>
+  <tr>
+    <td>6</td>
+    <td>Adequate moisture is necessary for effective potassium uptake; low soil water reduces K uptake by plant roots.</td>
+      </tr>
+<tr>
+<td>7</td>
+<td>Liming acidic soils can increase potassium retention in some soils by reducing leaching; practices that increase soil organic matter can also increase potassium retention.</td>
+</tr>
+<tr>
+<td>8</td>
+<td>Can try growing wheat,soyabean and peanuts.</td>
+</tr>
 
-        <br/>3.<i> Greensand </i> -which is available from most nurseries, will also add potassium to you garden.
 
-        <br/>4. Bury banana peels an inch below the soils surface
-
-        <br/>5. Use Potash fertilizers since they contain high values potassium
-
-        <br/>6. Adequate moisture is necessary for effective potassium uptake; low soil water reduces K uptake by plant roots.
-
-	<br/>7. Liming acidic soils can increase potassium retention in some soils by reducing leaching; practices that increase soil organic matter can also increase potassium retention.        
-	
-	<br/>8. Can try growing wheat,soyabean and peanuts.
+</table>
 """,
     }
